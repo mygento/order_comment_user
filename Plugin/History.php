dd811c2fb4c0be9856efa58dce2ba221aa14721d
Nikita Tarasov <nikita@mygento.ru>
2020-03-25 23:08:29 +0300

    should work

--- Plugin/History.php
+++ Plugin/History.php
@@ -11,10 +11,33 @@ namespace Mygento\OrderCommentUser\Plugin;
 class History
 {
     /**
+     * @var \Magento\Backend\Model\Auth\Session
+     */
+    private $session;
+
+    /**
+     * @param \Magento\Backend\Model\Auth\Session $session
+     */
+    public function __construct(
+        \Magento\Backend\Model\Auth\Session $session
+    ) {
+        $this->session = $session;
+    }
+
+    /**
      * @param \Magento\Sales\Model\Order\Status\History $subject
      */
     public function beforeBeforeSave($subject)
     {
-        $subject->setUsername('banan');
+        if (!$this->session->isLoggedIn()) {
+            return;
+        }
+
+        if (!$this->session->getUser()) {
+            return;
+        }
+
+        $user = $this->session->getUser();
+        $subject->setUsername($user->getName());
     }
 }
