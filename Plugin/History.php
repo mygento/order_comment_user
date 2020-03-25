c3636b16a66b04c6d58079ab7e81a11b3271441f
Nikita Tarasov <nikita@mygento.ru>
2020-03-25 23:38:47 +0300

    bump release

--- Plugin/History.php
+++ Plugin/History.php
@@ -40,4 +40,19 @@ class History
         $user = $this->session->getUser();
         $subject->setUsername($user->getName());
     }
+
+    /**
+     * @param \Magento\Sales\Model\Order\Status\History $subject
+     * @param string $result
+     *
+     * @return string
+     */
+    public function afterGetStatusLabel($subject, $result)
+    {
+        if ($subject->getUsername()) {
+            return implode('|', [$result, $subject->getUsername()]);
+        }
+
+        return $result;
+    }
 }
